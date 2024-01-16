2d11fc63faa07114feb1492e15d76565269f26d0
Rakhmad Fajar Ramadhan <71025577+rakhmadfajar@users.noreply.github.com>
2024-01-16 12:34:27 +0700

    Time: 1163 ms (22.56%), Space: 0B (100.00%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,24 @@
+SELECT
+    ROUND(SUM(tiv_2016),2) as tiv_2016
+FROM    
+    Insurance
+WHERE
+    (tiv_2015 IN (
+        SELECT
+            tiv_2015
+        FROM 
+            Insurance
+        GROUP BY
+            tiv_2015
+        HAVING 
+            COUNT(tiv_2015) > 1))
+    AND
+        CONCAT(lat, lon) NOT IN
+        (SELECT
+            CONCAT(lat,lon) 
+            FROM
+                Insurance
+            GROUP BY
+                CONCAT(lat,lon)
+            HAVING
+                COUNT(CONCAT(lat,lon)) > 1)
